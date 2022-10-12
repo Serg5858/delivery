f5bfb5e30537db3e695065d5ac357422e5cb6ef1
Serg5858 <ss3@ro.ru>
2022-10-12 17:59:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/6rt76nwwcv5mqax7?svg=true)](https://ci.appveyor.com/project/Serg5858/delivery)
---
+++ src/test/java/ru/netology/delivery/DeliveryTest.java
@@ -0,0 +1,37 @@
+package ru.netology.delivery;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+
+public class DeliveryTest {
+
+
+    String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @Test
+    void shouldGetDeliveryCard() {
+        String date = generateDate(3);
+        Selenide.open("http://localhost:9999");
+        $("[data-test-id = 'city'] input").setValue("Москва");
+        $("[data-test-id = 'date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id = 'date'] input").setValue(date);
+        $("[data-test-id= 'name'] input").setValue("Иванов Семен");
+        $("[data-test-id = 'phone'] input").setValue("+79007778990");
+        $("[data-test-id = 'agreement']").click();
+        $$("[type = button]").find(exactText("Забронировать")).click();
+        $("[data-test-id = 'notification']").shouldHave(exactText("Успешно! Встреча успешно забронирована на " + date), Duration.ofMillis(15000)).shouldBe(exist);
+    }
+}
